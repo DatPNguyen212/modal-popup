a19b1950fd9c485686e017d25889a039b9f28b87
Dat P Nguyen <darkexpert3@gmail.com>
2024-07-28 05:06:01 +0700

    add styling to change overlay color when button is clicked

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const listModalButtons = document.querySelectorAll('[data-modal-button]')
 const listModalCloseButtons = document.querySelectorAll('[modal-close-button]')
+const overlay = document.querySelector('[data-modal-overlay]')
 
 listModalButtons.forEach((button) => {
   button.addEventListener('click', (event) => {
@@ -13,4 +14,5 @@ function openModal(modal) {
     return
   }
   modal.classList.add('active')
+  overlay.classList.add('active')
 }
